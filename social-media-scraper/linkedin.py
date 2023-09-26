e71e1887fb1d2464e72a3b62da6cdce3706b0725
Johary <joharyandrianjafimanohisolo@gmail.com>
2023-09-26 07:56:56 +0300

    linkedin

--- social-media-scraper/linkedin.py
+++ social-media-scraper/linkedin.py
@@ -12,68 +12,68 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse, parse_qs
 from langdetect import detect
 import re
+from linkedin_scraper import actions
+import random
 
 
 class Linkedin(Scraping):
-    def __init__(self, url: str, establishment: str, site_url: str):
-        super().__init__(in_background=False, url=url, establishment=establishment, site_url=site_url)
+    def __init__(self, url: str, establishment: str):
+        super().__init__(in_background=False, url=url, establishment=establishment)
+        self.posts = []
+
+    def execute(self):
+        self.login()
+        time.sleep(random.randint(2, 10))
+        self.scrap(self.url)
+        time.sleep(random.randint(5, 10))
+        self.extract()
 
     def login(self):
-        self.scrap(self.site_url)
-        time.sleep(20)
-        # login_form = self.driver.find_element(By.ID, "loginForm")
-        # email_input = self.driver.find_element(By.NAME, "email-or-phone").send_keys("joharyandrianjafimanohisolo@gmail.com")
-        # password_input = self.driver.find_element(By.NAME, "password").send_keys("jOx3rBNT5Ax9")
-        # time.sleep(2)
-        # login_form.find_element(By.XPATH, "//button[@type='submit']").click()
-        # time.sleep(5)
-
-
-        # if self.driver.find_element(By.XPATH, "//div[@role='dialog']"):
-        #     
-
-
-    # def load_reviews(self) -> None:
-    #     self.driver.find_element(By.ID, 'avis-tout-cta').click()
-    #     results = int(''.join([x for x in self.driver.find_element(By.ID, 'avis-comp-content').find_element(By.CLASS_NAME, 'ml-1').text if x.isdigit()]))
-    #     for i in range(results//3):
-    #         self.driver.find_element(By.ID, 'avis-cards-content-container').click()
-    #         for k in range(3):
-    #             self.driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
-    #         time.sleep(2)
+        actions.login(self.driver, 'joharyandrianjafimanohisolo@gmail.com', 'jOx3rBNT5Ax9')        
 
     def extract(self) -> None:
         try:
-            pass
-            # time.sleep(10)
-            # soup  = BeautifulSoup(self.driver.page_source, 'lxml')
-            # nb_followers = soup.find_all('a', href=lambda href: href and "/followers/" in href)[0].find('span', {'title': True})['title']
-            # print(nb_followers)
-                    # self.load_reviews()
+            soup  = BeautifulSoup(self.driver.page_source, 'lxml')
+            infos = soup.find_all('div', {'class': 'org-top-card-summary-info-list__info-item'})
+            followers = int(''.join([item.text.strip() for item in infos if 'abonnés' in item.text.strip()][0].split()[:-1]))
+            print(followers)
 
-        # reviews = []
+            self.scrap(f"{self.url}/posts/?feedView=all")
+            time.sleep(random.randint(5, 10))
 
-        # try:
-        #     soup  = BeautifulSoup(self.driver.page_source, 'lxml')
-        #     followers = soup.find_all('a', href=lambda href: href and "/followers/" in href)[0].text.strip()
-        #     print(followers)
-        #     tab = followers.split(' ')
-        #     print(tab)
-        #     nb = tab[0].replace(',', ' ') + ('000' if len(tab) > 1 and tab[1] == 'K' else '')
-        #     print(nb)
-        #     # review_cards = soup.find('div', {'id':'avis-cards-content-container'}).find_all('div', {'typeof':'comment'})
+            posts = soup.find_all('div', {'class': 'ember-view'})
+            print(len(posts))
+            # for post in posts:
+            #     try:
+            #         self.posts.append({
+            #             'title': post.find('div', {'class': 'update-components-update-v2__commentary'}).text.strip(),
+            #             'comments': post.find('li', {'class': 'social-details-social-counts__comments'}).text.strip(),
+            #             'share': post.find('li', {'class': 'social-details-social-counts__item--with-social-proof'}).text.strip(),
+            #             'likes': post.find('span', {'class': 'social-details-social-counts__reactions-count'}).text.strip(),
+            #             'publishedAt': post.find('a', {'class': 'app-aware-link'})['aria-label']
+            #         })
+            #     except Exception as e:
+            #         print(e)
+
+            print(self.posts)
+
+            # tab = followers.split(' ')
+            # print(tab)
+            # nb = tab[0].replace(',', ' ') + ('000' if len(tab) > 1 and tab[1] == 'K' else '')
+            # print(nb)
+            # review_cards = soup.find('div', {'id':'avis-cards-content-container'}).find_all('div', {'typeof':'comment'})
             
-        #     # for review in review_cards:
-        #     #     date = review.find('span', {'property':'dateCreated'})['content']
-        #     #     data = {}
-        #     #     data['author'] = review.find('div', class_='date-publication').find('strong').text.strip()
-        #     #     data['date_review'] = '/'.join(date.split('-')[::-1])
-        #     #     data['comment'] = review.find('p', class_='avis-comment').text.strip() if review.find('p', class_='avis-comment') else ''
-        #     #     data['rating'] = review.find('span', class_='score-text').text if review.find('span', class_='score-text') else 0
-        #     #     data['language'] = detect(data['comment'])
-        #     #     data['source'] = urlparse(self.url).netloc.split('.')[1]
-        #     #     data['establishment'] = f'/api/establishments/{self.establishment}'
-        #     #     reviews.append(data)
+            # for review in review_cards:
+            #     date = review.find('span', {'property':'dateCreated'})['content']
+            #     data = {}
+            #     data['author'] = review.find('div', class_='date-publication').find('strong').text.strip()
+            #     data['date_review'] = '/'.join(date.split('-')[::-1])
+            #     data['comment'] = review.find('p', class_='avis-comment').text.strip() if review.find('p', class_='avis-comment') else ''
+            #     data['rating'] = review.find('span', class_='score-text').text if review.find('span', class_='score-text') else 0
+            #     data['language'] = detect(data['comment'])
+            #     data['source'] = urlparse(self.url).netloc.split('.')[1]
+            #     data['establishment'] = f'/api/establishments/{self.establishment}'
+            #     reviews.append(data)
 
         except Exception as e:
             print('extraction file')
@@ -82,5 +82,5 @@ class Linkedin(Scraping):
         # self.data = reviews
 
 
-instance = Linkedin(url="https://www.linkedin.com/company/chateau-de-candie", establishment=2, site_url="https://www.linkedin.com/")
+instance = Linkedin(url="https://www.linkedin.com/company/dream-hotel-group/", establishment=2)
 instance.execute()
