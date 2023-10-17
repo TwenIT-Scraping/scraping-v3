39abaa15e4f7f782a47a3dd32b42bc33a701188c
Johary <joharyandrianjafimanohisolo@gmail.com>
2023-10-17 10:09:45 +0300

    test cron

--- meteo-scraper/test.py
+++ meteo-scraper/test.py
@@ -3,5 +3,5 @@ from datetime import datetime
 
 if __name__ == '__main__':
     now = datetime.now()
-    with open('test.txt', 'w', encoding='utf-8') as file:
+    with open('/var/www/scraping-v3/meteo-scraper/test.txt', 'w', encoding='utf-8') as file:
         file.write(now.strftime("%d/%m/%Y %H:%M:%S"))
