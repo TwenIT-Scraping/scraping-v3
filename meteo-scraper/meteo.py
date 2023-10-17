ba440c38488ea330b3e2a7633c3bf7b92a898c4e
Johary <joharyandrianjafimanohisolo@gmail.com>
2023-10-17 12:14:30 +0300

    final meteo scrap program

--- meteo-scraper/meteo.py
+++ meteo-scraper/meteo.py
@@ -365,40 +365,54 @@ if __name__ == '__main__':
     if os.path.exists('/var/www/scraping-v3/meteo-scraper/meteo-scraping-log.txt'):
         with open('/var/www/scraping-v3/meteo-scraper/meteo-scraping-log.txt', 'a', encoding='utf-8') as file:
             file.write("Démarrage scrap meteo: " +
-                       now.strftime("%d/%m/%Y %H:%M:%S") + '\n')
+                       now.strftime("%d/%m/%Y %H:%M:%S"))
     else:
         with open('/var/www/scraping-v3/meteo-scraper/meteo-scraping-log.txt', 'w', encoding='utf-8') as file:
             file.write("Démarrage scrap meteo: " +
-                       now.strftime("%d/%m/%Y %H:%M:%S") + '\n')
+                       now.strftime("%d/%m/%Y %H:%M:%S"))
 
-    args = main_arguments()
+    try:
 
-    miss = check_arguments(args)
+        args = main_arguments()
 
-    if not len(miss):
+        miss = check_arguments(args)
 
-        success = False
+        if not len(miss):
 
-        try:
-            d = MeteoLocalityScraper('locality_log', 'locality')
-            d.initialize()
-            time.sleep(2)
-            m = MeteoAPIScraper('meteo_log', 'locality',
-                                'meteo_url', 'meteo_data')
-            if args.dates:
-                m.set_dates(args.dates.split(','))
-            else:
-                m.set_dates([datetime.now().strftime('%Y-%m-%d')])
-
-            m.set_key_index(int(args.key))
-            m.start()
-            m.upload()
-            success = True
-        except:
             success = False
 
-    if success:
-        clean()
+            try:
+                d = MeteoLocalityScraper('locality_log', 'locality')
+                d.initialize()
+                time.sleep(2)
+                m = MeteoAPIScraper('meteo_log', 'locality',
+                                    'meteo_url', 'meteo_data')
+                if args.dates:
+                    m.set_dates(args.dates.split(','))
+                else:
+                    m.set_dates([datetime.now().strftime('%Y-%m-%d')])
 
-    else:
-        raise Exception(f"Argument(s) manquant(s): {', '.join(miss)}")
+                m.set_key_index(int(args.key))
+                m.start()
+                m.upload()
+                success = True
+            except:
+                success = False
+
+        if success:
+            clean()
+
+            now = datetime.now()
+
+            with open('/var/www/scraping-v3/meteo-scraper/meteo-scraping-log.txt', 'w', encoding='utf-8') as file:
+                file.write("  ===>  Fin scrap meteo: " +
+                           now.strftime("%d/%m/%Y %H:%M:%S") + '\n')
+
+        else:
+            raise Exception(f"Argument(s) manquant(s): {', '.join(miss)}")
+
+    except Exception as e:
+        now = datetime.now()
+        with open('/var/www/scraping-v3/meteo-scraper/meteo-scraping-log.txt', 'w', encoding='utf-8') as file:
+            file.write("  ===>  Fin scrap meteo WITH ERRORS: " +
+                       now.strftime("%d/%m/%Y %H:%M:%S") + ':' + e + '\n')
