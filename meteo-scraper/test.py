6890db04f3f90ce1c563ca1f38bed663a11495b5
Johary <joharyandrianjafimanohisolo@gmail.com>
2023-10-17 10:27:04 +0300

    améliore test

--- meteo-scraper/test.py
+++ meteo-scraper/test.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     now = datetime.now()
     if os.path.exists('/var/www/scraping-v3/meteo-scraper/test.txt'):
         with open('/var/www/scraping-v3/meteo-scraper/test.txt', 'a', encoding='utf-8') as file:
-            file.write(now.strftime("%d/%m/%Y %H:%M:%S"))
+            file.write(now.strftime("%d/%m/%Y %H:%M:%S")+'\n')
     else:
         with open('/var/www/scraping-v3/meteo-scraper/test.txt', 'w', encoding='utf-8') as file:
-            file.write(now.strftime("%d/%m/%Y %H:%M:%S"))
+            file.write(now.strftime("%d/%m/%Y %H:%M:%S")+'\n')
