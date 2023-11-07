85e36192a504e06cfd4dea5d1b0c472935a2037a
Johary97 <joharyandrianjafimanohisolo@gmail.com>
2023-11-07 21:49:48 +0100

    hide trustpilot driver

--- review-scraper/trustpilot.py
+++ review-scraper/trustpilot.py
@@ -20,7 +20,7 @@ from langdetect import detect
 
 class Trustpilot(Scraping):
     def __init__(self, url: str, establishment: str):
-        super().__init__(in_background=False, url=url, establishment=establishment)
+        super().__init__(in_background=True, url=url, establishment=establishment)
 
     def extract(self):
 
