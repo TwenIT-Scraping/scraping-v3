78d46bd555ee8cfabf76a4e65cee5faaa446154e
Johary <joharyandrianjafimanohisolo@gmail.com>
2023-11-20 11:15:24 +0300

    resolve booking

--- review-scraper/booking.py
+++ review-scraper/booking.py
@@ -34,83 +34,87 @@ class Booking(Scraping):
         #     By.XPATH, "//select[@id='sorting']"))
         # review_order.select_by_value('completed_desc')
 
-        view_list_btn = self.driver.find_element(
-            By.XPATH, "//div[@class='review_list_nav_wrapper clearfix']/form/input[@type='submit']")
-        self.driver.execute_script("arguments[0].click();", view_list_btn)
+        try:
+            view_list_btn = self.driver.find_element(
+                By.XPATH, "//div[@class='review_list_nav_wrapper clearfix']/form/input[@type='submit']")
+            self.driver.execute_script("arguments[0].click();", view_list_btn)
 
-        while True:
-            time.sleep(10)
+            while True:
+                time.sleep(10)
 
-            page = self.driver.page_source
+                page = self.driver.page_source
 
-            soupe = BeautifulSoup(page, 'lxml')
+                soupe = BeautifulSoup(page, 'lxml')
 
-            review_cards = soupe.find_all('li', {'itemprop': 'review'})
+                review_cards = soupe.find_all('li', {'itemprop': 'review'})
 
-            for card in review_cards:
-                title = card.find('div', {'class': 'review_item_header_content'}).text.strip(
-                ) if card.find('div', {'class': 'review_item_header_content'}) else ""
-                negative = card.find('p', {'class': 'review_neg'}).text.strip(
-                ) if card.find('p', {'class': 'review_neg'}) else ""
-                positive = card.find('p', {'class': 'review_pos'}).text.strip(
-                ) if card.find('p', {'class': 'review_pos'}) else ""
-                detail = f'{positive} | {negative}' if positive and negative else (
-                    positive if positive else negative)
-                comment = f"{title}{': ' if title and detail else ''}{detail}"
+                for card in review_cards:
+                    title = card.find('div', {'class': 'review_item_header_content'}).text.strip(
+                    ) if card.find('div', {'class': 'review_item_header_content'}) else ""
+                    negative = card.find('p', {'class': 'review_neg'}).text.strip(
+                    ) if card.find('p', {'class': 'review_neg'}) else ""
+                    positive = card.find('p', {'class': 'review_pos'}).text.strip(
+                    ) if card.find('p', {'class': 'review_pos'}) else ""
+                    detail = f'{positive} | {negative}' if positive and negative else (
+                        positive if positive else negative)
+                    comment = f"{title}{': ' if title and detail else ''}{detail}"
 
-                raw_date = card.find('p', {'class': 'review_item_date'}).text.strip(
-                ) if card.find('p', {'class': 'review_item_date'}) else ""
-                dates = raw_date.split()
+                    raw_date = card.find('p', {'class': 'review_item_date'}).text.strip(
+                    ) if card.find('p', {'class': 'review_item_date'}) else ""
+                    dates = raw_date.split()
+                    try:
+                        date_review = f"{dates[-3]}/{month_number(dates[-2], 'fr')}/{dates[-1]}"
+                    except Exception as e:
+                        date_review = f"{dates[-3]}/{month_number(dates[-2], 'en')}/{dates[-1]}"
+                    if card.find('p', {'class': 'review_staydate '}):
+                        date_visit_raw = card.find(
+                            'p', {'class': 'review_staydate '}).text.strip().split()[-2:]
+                        date_visit = f"{(datetime().day-1)}/{month_number(date_visit_raw[0], 'en')}/{date_visit_raw[1]}"
+                        print(date_visit)
+                    else:
+                        date_visit = date_review
+
+                    try:
+                        lang = detect(comment)
+                    except:
+                        lang = 'en'
+
+                    try:
+                        reviews.append({
+                            'comment': comment,
+                            'rating': card.find('span', {'class': 'review-score-badge'}).text.strip()
+                            if card.find('span', {'class': 'review-score-badge'}) else "0",
+                            'date_review': date_review,
+                            'language': lang,
+                            'source': urlparse(self.url).netloc.split('.')[1],
+                            'author': card.find('p', {'class': 'reviewer_name'}).text.strip() if card.find('p', {'class': 'reviewer_name'}) else "",
+                            'establishment': f'/api/establishments/{self.establishment}',
+                            'settings': f'/api/settings/{self.settings}',
+                            'date_visit': date_review,
+                            'novisitday': "0"
+                        })
+                    except Exception as e:
+                        print(e)
+                        continue
+
+                # if not self.check_date(reviews[-1]['date_review']):
+                #     break
+
                 try:
-                    date_review = f"{dates[-3]}/{month_number(dates[-2], 'fr')}/{dates[-1]}"
+
+                    next_btn = self.driver.find_element(
+                        By.ID, 'review_next_page_link')
+
+                    if next_btn:
+                        self.driver.execute_script(
+                            "arguments[0].click();", next_btn)
+                        time.sleep(4)
+
                 except Exception as e:
-                    date_review = f"{dates[-3]}/{month_number(dates[-2], 'en')}/{dates[-1]}"
-                if card.find('p', {'class': 'review_staydate '}):
-                    date_visit_raw = card.find(
-                        'p', {'class': 'review_staydate '}).text.strip().split()[-2:]
-                    date_visit = f"{(datetime().day-1)}/{month_number(date_visit_raw[0], 'en')}/{date_visit_raw[1]}"
-                    print(date_visit)
-                else:
-                    date_visit = date_review
+                    break
 
-                try:
-                    lang = detect(comment)
-                except:
-                    lang = 'en'
-
-                try:
-                    reviews.append({
-                        'comment': comment,
-                        'rating': card.find('span', {'class': 'review-score-badge'}).text.strip()
-                        if card.find('span', {'class': 'review-score-badge'}) else "0",
-                        'date_review': date_review,
-                        'language': lang,
-                        'source': urlparse(self.url).netloc.split('.')[1],
-                        'author': card.find('p', {'class': 'reviewer_name'}).text.strip() if card.find('p', {'class': 'reviewer_name'}) else "",
-                        'establishment': f'/api/establishments/{self.establishment}',
-                        'settings': f'/api/settings/{self.settings}',
-                        'date_visit': date_review,
-                        'novisitday': "0"
-                    })
-                except Exception as e:
-                    print(e)
-                    continue
-
-            # if not self.check_date(reviews[-1]['date_review']):
-            #     break
-
-            try:
-
-                next_btn = self.driver.find_element(
-                    By.ID, 'review_next_page_link')
-
-                if next_btn:
-                    self.driver.execute_script(
-                        "arguments[0].click();", next_btn)
-                    time.sleep(4)
-
-            except Exception as e:
-                break
+        except:
+            pass
 
         self.data = reviews
 
