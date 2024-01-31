97f4e82092cadea79a7b38b8f7560ede83f251db
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-01-31 13:02:47 +0300

    programme review categorizer

--- review-categorizer/__main__.py
+++ review-categorizer/__main__.py
@@ -1,7 +1,7 @@
 import csv
 import pprint
-import urllib3
 import json
+import random
 import pandas as pd
 from datetime import datetime, timedelta
 from random import randint
@@ -14,6 +14,7 @@ import dotenv
 import argparse
 import ssl
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from api import ERApi
 
 dotenv.load_dotenv()
 
@@ -22,16 +23,113 @@ def main_arguments() -> object:
     parser = argparse.ArgumentParser(description="Programme catégorisation",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--type', '-t', dest='type', default='',
-                        help="""Options: comments, reviews""")
+                        help="""Options: comment, review""")
     parser.add_argument('--env', '-v', dest='env', default="PROD",
                         help="Optionnel: environnement de l'api. PROD par défaut")
     return parser.parse_args()
 
 
 ARGS_INFO = {
-    '-t': {'long': '--type', 'dest': 'type', 'help': "Options: comments, reviews"},
+    '-t': {'long': '--type', 'dest': 'type', 'help': "Options: comment, review"},
     '-v': {'long': '--env', 'dest': 'env', 'help': "Optionnel: environnement de l'api. PROD par défaut"}
 }
+
+
+class ClassificationAPI(object):
+
+    def __init__(self, env='dev', type='review', tag='', limit=50) -> None:
+        self.type = type
+        self.establishment = None
+        self.categories = []
+        self.tag = tag
+        self.lines = []
+        self.env = env
+        self.limit = limit
+
+    def fetch_datas(self):
+        try:
+            get_instance = ERApi(
+                method="get", entity=f"establishment/{self.tag}/reviews_to_classify", env=self.env, params={'limit': self.limit})
+            res = get_instance.execute()
+            pages = 1
+            page = 2
+
+            if (res):
+                self.categories = res['categories']
+                self.establishment = res['establishment']
+                pages = res['pages']
+                self.lines = res['reviews']
+                print("Etablissement traité: ", res['establishment']['name'])
+                print("Liste des catégories disponibles: ", res['categories'])
+                print("Nombre total lignes à traiter: ", res['count'])
+
+            while (page <= pages):
+                get_instance = ERApi(
+                    method="get", entity=f"establishment/{self.tag}/reviews_to_classify", env=self.env, params={'page': page, 'limit': self.limit})
+                res = get_instance.execute()
+                self.lines += res['reviews']
+                page += 1
+                time.sleep(1)
+
+            print("Lignes récupérées: ", len(self.lines))
+
+        except Exception as e:
+            print(e)
+
+    def check_categories(self, line):
+        try:
+            # classifier = pipeline(task="zero-shot-classification",
+            #                       device=0, model="facebook/bart-large-mnli")
+
+            # prediction = classifier(
+            #     line['text'], self.categories, multi_class=False)
+
+            prediction = {
+                # 'labels': ['travel', 'cooking', 'dancing'],
+                'labels': self.categories,
+                # 'scores': [random.uniform(0, 1) for i in range(3)],
+                'scores': [random.uniform(0, 1) for i in range(len(self.categories))],
+                'sequence': line['text']
+            }
+
+            line['prediction'] = prediction
+
+            return line
+
+        except Exception as e:
+            print(e)
+
+    def update_lines(self):
+        for line in self.lines:
+            line = self.check_categories(line)
+
+    def transform_data(self):
+        result = ""
+        for line in self.lines:
+            for i in range(0, len(line['prediction']['labels'])):
+                if line['prediction']['scores'][i] >= 0.9:
+                    result += f"{self.type}&{line['prediction']['labels'][i]['category']}&{line['prediction']['scores'][i]}&{line['id']}#"
+
+        return result
+
+    def upload(self):
+        try:
+            data = self.transform_data()
+            post_instance = ERApi(
+                method="postclassifications", entity=f"classification/multi", env=self.env, body={'data_content': data})
+            return post_instance.execute()
+        except Exception as e:
+            print(e)
+
+    def execute(self):
+        try:
+            self.fetch_datas()
+            self.update_lines()
+            # res = self.transform_data()
+            res = self.upload()
+            print(res)
+        except Exception as e:
+            print(e)
 
 
 def check_arguments(args):
@@ -41,323 +139,6 @@ def check_arguments(args):
             f'-t ou {ARGS_INFO["-t"]["long"]} ({ARGS_INFO["-t"]["help"]})')
 
     return miss
-
-
-def check_categories(text, labels):
-    try:
-        classifier = pipeline(task="zero-shot-classification",
-                              device=0, model="facebook/bart-large-mnli")
-
-        predictions = classifier(text, labels, multi_class=False)
-        print(predictions)
-    except Exception as e:
-        print(e)
-
-# def clean():
-#     files = ['locality_log.json', 'locality.csv', 'meteo_data.txt',
-#              'meteo_log.json', 'meteo_url.json']
-#     for file in files:
-#         if os.path.exists(f'{os.environ.get("HISTORY_FOLDER")}/{file}'):
-#             os.remove(file)
-
-
-# class MeteoAPI(object):
-
-#     def __init__(self, logfile: str, env: str) -> None:
-
-#         self.logfile = logfile
-#         self.nexties_api_key = os.environ.get(f"API_TOKEN_{env.upper()}")
-#         self.nexties_api_url = os.environ.get(f"API_URL_{env.upper()}")
-#         self.headers = {'Authorization': self.nexties_api_key,
-#                         'Content-Type': 'application/json'}
-#         self.history = {}
-#         self.http = None
-
-#     def get_request(self, url: str, header: bool = False) -> dict:
-#         try:
-#             response = None
-#             self.http = urllib3.PoolManager(cert_reqs='CERT_NONE')
-
-#             if header:
-#                 response = self.http.request(
-#                     method='GET',
-#                     url=url,
-#                     headers=self.headers,
-#                     timeout=120
-#                 )
-#             else:
-#                 response = self.http.request(
-#                     method='GET',
-#                     url=url,
-#                     timeout=120
-#                 )
-
-#             return orjson.loads(response.data)
-
-#         except urllib3.exceptions.HTTPError as e:
-#             print(e)
-#             print('==> Connexion failed!!')
-#             print(e.reason)
-#             sys.exit()
-
-#     def post_request(self, url: str, body: str) -> dict:
-#         self.attempt_post = 1
-#         self.http = urllib3.PoolManager(cert_reqs=ssl.CERT_NONE)
-#         try:
-#             response = self.http.request(
-#                 method='POST',
-#                 url=url,
-#                 body=json.dumps(body),
-#                 headers=self.headers
-#             )
-#             if response.status != 200:
-#                 print("Post code: ", response.status)
-#             else:
-#                 print("Upload successfully !!!")
-
-#         except urllib3.exceptions.HTTPError as e:
-#             print("Erreur post")
-#             print(e.reason)
-
-#     def set_log(self, log_key: str, value: object) -> None:
-#         logs = {}
-#         try:
-#             with open(f'{self.logfile}.json', 'r') as openfile:
-#                 logs = json.load(openfile)
-#                 logs[log_key] = value
-#         except FileNotFoundError:
-#             print('==> Log file not found')
-#             sys.exit()
-
-#         with open(f'{self.logfile}.json', 'w') as outfile:
-#             outfile.write(json.dumps(logs, indent=4))
-#         self.load_history()
-
-#     def get_log(self, log_key: str) -> object:
-#         try:
-#             self.load_history()
-#             return self.history[log_key]
-#         except Exception as e:
-#             print(f'errors : {e}')
-#             sys.exit()
-
-#     def load_history(self) -> None:
-#         try:
-#             with open(f'{self.logfile}.json', 'r') as openfile:
-#                 self.history = json.load(openfile)
-#         except Exception as e:
-#             print(f'error: {e}')
-
-#     @abstractmethod
-#     def create_logfile(self) -> None:
-#         pass
-
-#     @abstractmethod
-#     def save(self):
-#         pass
-
-
-# class MeteoLocalityScraper(MeteoAPI):
-
-#     def __init__(self, logfile: str, file_output: str, env: str) -> None:
-#         super().__init__(logfile, env=env)
-#         self.file_output = file_output
-#         self.localities = []
-#         self.etab_loaded = False
-
-#     def create_logfile(self) -> None:
-#         log = {
-#             'total_page': 2,
-#             'last_page': 1,
-#         }
-#         if not os.path.exists(self.logfile):
-#             with open(f"{self.logfile}.json", "w") as logfile:
-#                 logfile.write(json.dumps(log))
-
-#     def get_localities(self) -> None:
-#         print('==> loading all localities ...')
-#         has_next = True
-#         page = 1
-
-#         while has_next:
-#             result = self.get_request(
-#                 f"{self.nexties_api_url}/localities?page={page}", header=True)
-
-#             if 'hydra:view' in result.keys() and 'hydra:next' in result['hydra:view']:
-#                 has_next = True
-#             else:
-#                 has_next = False
-
-#             for item in result['hydra:member']:
-#                 if item['gps']:
-#                     location = item['gps'].split(',')
-#                     self.localities.append({
-#                         'id': item['id'],
-#                         'lat': location[0],
-#                         'long': location[1],
-#                         'name': item['name']
-#                     })
-
-#             page += 1
-
-#         self.save()
-
-#     def save(self) -> None:
-#         print('==> saving data ...')
-#         self.localities_file = f"{self.file_output}.csv"
-#         df = pd.DataFrame(self.localities)
-#         df.to_csv(self.localities_file, mode='w', header=True, index=False)
-#         self.localities.clear()
-
-#     def initialize(self):
-#         self.create_logfile()
-#         self.get_localities()
-#         print("==> All establishement loaded ('_')")
-
-
-# class MeteoAPIScraper(MeteoAPI):
-
-#     def __init__(self, logfile: str, data_source: str, url_file: str, output_file: str, env: str) -> None:
-#         super().__init__(logfile, env=env)
-#         self.data_source = data_source
-#         self.url_file = url_file
-#         self.output_file = output_file
-#         self.api_keys = os.environ.get("METEO_API_KEYS").split(',')
-#         self.urls = []
-#         self.dates = []
-#         self.meteo_key = 0
-
-#     def set_key_index(self, key):
-#         self.meteo_key = key
-
-#     def set_dates(self, dates):
-#         if len(dates) == 2:
-#             start_dt = datetime.strptime(dates[0], "%Y-%m-%d")
-#             end_dt = datetime.strptime(dates[1], "%Y-%m-%d")
-#             delta = timedelta(days=1)
-
-#             # store the dates between two dates in a list
-#             tmp = []
-
-#             while start_dt <= end_dt:
-#                 # add current date to list by converting  it to iso format
-#                 tmp.append(start_dt.strftime("%Y-%m-%d"))
-#                 # increment start date by timedelta
-#                 start_dt += delta
-
-#             self.dates = tmp
-
-#         else:
-#             self.dates = dates
-
-#     def format_url(self, data: dict, date: str) -> str:
-#         api_key = self.api_keys[self.meteo_key]
-#         return {'locality_id': data['id'], 'url': f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{data['lat']}%2C{data['long']}/{date}/{date}/?unitGroup=us&key={api_key}&contentType=json"}
-
-#     def create_logfile(self) -> None:
-#         print('==> creating logfile !!')
-#         time.sleep(0.5)
-#         log = {'last_index': 0, 'total_index': 0, 'api_keys_usage': {}}
-#         if not os.path.exists(self.logfile):
-#             with open(f"{self.logfile}.json", "w") as logfile:
-#                 logfile.write(json.dumps(log))
-
-#     def setup_config(self) -> None:
-#         print('==> configuring urls ...')
-#         time.sleep(.3)
-#         if os.path.exists(self.data_source):
-#             if not os.path.exists(f'{self.url_file}.json'):
-#                 data = []
-#                 data_source = pd.read_csv(self.data_source)
-
-#                 counter = 0
-
-#                 for x in range(len(data_source)):
-#                     for date in self.dates:
-#                         counter += 1
-#                         url = self.format_url(
-#                             data_source.loc[x].to_dict(), date)
-#                         data.append(url)
-
-#                         if counter == 45:
-#                             counter = 0
-#                             next_index = self.meteo_key + 1 if self.meteo_key + \
-#                                 1 < len(self.api_keys) else 0
-#                             self.set_key_index(next_index)
-
-#                 with open(f'{self.url_file}.json', 'w') as openfile:
-#                     openfile.write(json.dumps(data, indent=4))
-
-#                 print('==> configuring url files done ...')
-#             else:
-#                 print('==> url file already exist ...')
-
-#         else:
-#             print('==> data source not found ...')
-#         time.sleep(.5)
-
-#     def load_urls(self) -> None:
-#         print('==> Loading urls ...')
-#         time.sleep(.5)
-#         with open(f'{self.url_file}.json', 'r') as openfile:
-#             self.urls = json.loads(openfile.read())
-
-#     def extract(self, data: dict) -> dict:
-#         print('==> extracting data ...')
-#         response = data['days'][0]
-#         return {
-#             'locality': str(data['locality_id']),
-#             'dateWeather': response['datetime'],
-#             'timeWeather':  response['datetime'],
-#             'tempmax': response['tempmax'],
-#             'tempmin': response['tempmin'],
-#             'temp': response['temp'],
-#             'dew': response['dew'],
-#             'humidity': response['humidity'],
-#             'snow': response['snow'],
-#             'windspeed': response['windspeed'],
-#             'cloudcover': response['cloudcover'],
-#             'sunrise': f"{response['datetime']} {response['sunrise']}",
-#             'sunset': f"{response['datetime']} {response['sunset']}",
-#             'conditions': response['conditions'],
-#             'description': response['description']
-#         }
-
-#     def save(self, data: dict):
-#         print('==> saving data ...')
-
-#         result = f"\n{data['locality']}${data['dateWeather']}${data['timeWeather']}${data['tempmax']}${data['tempmin']}${data['temp']}${data['dew']}${data['humidity']}${data['snow']}${data['windspeed']}${data['cloudcover']}${data['sunrise']}${data['sunset']}${data['conditions']}${data['description']}#"
-#         with open(f'{self.output_file}.txt', 'a') as filesave:
-#             filesave.write(result)
-
-#     def upload(self):
-
-#         text = ""
-
-#         with open(f'{self.output_file}.txt', 'r') as file:
-#             csvreader = csv.reader(file)
-#             for row in csvreader:
-#                 if len(row):
-#                     text += ''.join(row)
-
-#         self.post_request(
-#             f"{self.nexties_api_url}/weatthers/upload", {'data_content': text})
-
-#     def start(self):
-#         self.create_logfile()
-#         self.setup_config()
-#         self.load_urls()
-#         self.load_history()
-#         total = len(self.urls)
-
-#         for x in range(total):
-#             print(
-#                 f"==> locality {x+1} / {total}")
-#             req_data = self.get_request(self.urls[x]['url'])
-#             req_data['locality_id'] = self.urls[x]['locality_id']
-#             clean_data = self.extract(req_data)
-#             self.save(clean_data)
-#             time.sleep(.5)
 
 
 if __name__ == '__main__':
@@ -387,22 +168,17 @@ if __name__ == '__main__':
             datetime_now = datetime.now().strftime("%Y-%m-%d %H_%M")
 
             if args.type == "reviews":
-                check_categories("Exceptionnel: L'emplacement très proche des pistes | Le mobilier et équipements de la cuisine et de la salle à manger sont certes très propres mais un peu vieillissants", [
-                                 "Emplacement", "équipement", "mobilier", "propreté", "visibilité", "nature"])
-            # d = MeteoLocalityScraper(
-            #     f'{os.environ.get("HISTORY_FOLDER")}/meteo/locality_log_{datetime_now}', f'{os.environ.get("HISTORY_FOLDER")}/locality_{datetime_now}', env=args.env)
-            # d.initialize()
-            # time.sleep(2)
-            # m = MeteoAPIScraper(f'{os.environ.get("HISTORY_FOLDER")}/meteo/locality_log_{datetime_now}', d.localities_file,
-            #                     f'{os.environ.get("HISTORY_FOLDER")}/meteo/meteo_url_{datetime_now}', f'{os.environ.get("HISTORY_FOLDER")}/meteo_data_{datetime_now}', env=args.env)
-            # if args.dates:
-            #     m.set_dates(args.dates.split(','))
-            # else:
-            #     m.set_dates([datetime.now().strftime('%Y-%m-%d')])
+                # check_categories("Exceptionnel: L'emplacement très proche des pistes | Le mobilier et équipements de la cuisine et de la salle à manger sont certes très propres mais un peu vieillissants", [
+                #                  "Emplacement", "équipement", "mobilier", "propreté", "visibilité", "nature"])
+                cl = ClassificationAPI(tag="645de52f135e8")
+                # cl.check_categories({
+                #     "id": 48020,
+                #     "text": "Exceptionnel: L'emplacement très proche des pistes | Le mobilier et équipements de la cuisine et de la salle à manger sont certes très propres mais un peu vieillissants"
+                # })
+                # cl.fetch_datas()
 
-            # m.set_key_index(int(args.key))
-            # m.start()
-            # m.upload()
+                cl.execute()
+
         except Exception as e:
             now = datetime.now()
             with open(history_filename, 'a', encoding='utf-8') as file:
---
+++ review-categorizer/api.py
@@ -0,0 +1,146 @@
+import requests
+import json
+from tkinter import messagebox
+from datetime import datetime
+import shutil
+import dotenv
+import os
+from urllib3 import encode_multipart_formdata
+
+
+class ERApi:
+
+    def __init__(self, env, method="get", entity="", params={}, body={}, id=-1):
+        dotenv.load_dotenv()
+
+        self.method = method
+        self.entity = entity
+        token = os.environ.get(f'API_TOKEN_{env.upper()}')
+        self.headers = {'Accept': 'application/json', 'Authorization': token}
+        self.params = params
+        self.body = body
+        self.id = id
+        self.api_url = os.environ.get(f"API_URL_{env.upper()}")
+        self.media_dir = os.environ.get("MEDIA_DIR")
+
+    def set_id(self, id):
+        self.id = id
+
+    def set_body(self, body):
+        self.body = body
+
+    def set_entity(self, entity):
+        self.entity = entity
+
+    def add_file(self, files):
+        self.files = files
+
+    def add_header(self, header):
+        for key in header.keys():
+            self.headers[key] = header[key]
+
+    def add_params(self, param):
+        for key in param.keys():
+            self.params[key] = param[key]
+
+    def execute(self):
+        response = {}
+        if self.method == 'delete':
+            if self.id != -1:
+                response = getattr(requests, self.method)(
+                    f'{self.api_url}{self.entity}/{self.id}',
+                    params=self.params,
+                    headers=self.headers
+                )
+                return response
+            else:
+                messagebox.askyesno(
+                    "Information", "Identifiant non spécifié!!!")
+
+        elif self.method == 'patch' or self.method == 'put':
+            if self.id != -1:
+                self.add_header({"Content-Type": "application/json"})
+                response = getattr(requests, self.method)(
+                    f'{self.api_url}{self.entity}/{self.id}',
+                    params=self.params,
+                    headers=self.headers,
+                    data=json.dumps(self.body)
+                )
+                return response
+            else:
+                messagebox.askyesno(
+                    "Information", "Identifiant non spécifié!!!")
+
+        elif self.method == 'getone':
+            if self.id != -1:
+                response = getattr(requests, 'get')(
+                    f'{self.api_url}{self.entity}/{self.id}',
+                    params=self.params,
+                    headers=self.headers
+                )
+            else:
+                messagebox.askyesno(
+                    "Information", "Identifiant non spécifié!!!")
+
+        elif self.method == 'postclassifications':
+
+            url = f'{self.api_url}classification/multi'
+            self.add_header({"Content-Type": "application/json"})
+            headers = self.headers
+
+            response = requests.request(
+                "POST", url, headers=headers, data=json.dumps(self.body), verify=False)
+
+            if response:
+                print(response.json())
+
+                return response
+
+        else:
+            self.add_header({"Content-Type": "application/json"})
+            response = getattr(requests, self.method)(
+                f'{self.api_url}{self.entity}',
+                params=self.params,
+                headers=self.headers,
+                data=json.dumps(self.body),
+                verify=False
+            )
+
+        if response.status_code >= 400:
+            response.raise_for_status()
+
+        return response and response.json()
+
+    @staticmethod
+    def delete_multi(entity, ids):
+        delete_instance = ERApi("delete", entity)
+        for item in ids:
+            delete_instance.set_id(item)
+            try:
+                r = delete_instance.execute()
+            except:
+                pass
+
+    @staticmethod
+    def get_all(entity):
+        all_data = []
+        page = 1
+
+        while True:
+            getreq = ERApi('get', entity)
+            getreq.add_params({'page': page})
+            results = getreq.execute()
+            all_data += results
+            page += 1
+            if len(results) == 0:
+                break
+
+        return all_data
+
+    @staticmethod
+    def delete_all(entity):
+        all_ids = [item['id'] for item in ERApi.get_all(entity)]
+        delete_req = ERApi(method="delete", entity=entity)
+        for item in all_ids:
+            delete_req.set_id(item)
+            delete_req.execute()
