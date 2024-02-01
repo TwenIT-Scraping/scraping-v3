3a4e914a218fba9874b115d76d311156ac4710a4
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-02-01 11:45:08 +0300

    programme review categorizer

--- review-categorizer/__main__.py
+++ review-categorizer/__main__.py
@@ -87,7 +87,7 @@ class ClassificationAPI(object):
                                   device=-1, model="facebook/bart-large-mnli")
 
             prediction = classifier(
-                line['text'], self.categories, multi_class=False)
+                line['text'], list(map(lambda x: x['category'], self.categories)), multi_class=False)
 
             # prediction = {
             #     # 'labels': ['travel', 'cooking', 'dancing'],
