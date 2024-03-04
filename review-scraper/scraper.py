d8230749adc0dcbd40ebf7404ce8fcb17e36db63
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-03-04 21:19:17 +0300

    lang

--- review-scraper/scraper.py
+++ review-scraper/scraper.py
@@ -203,6 +203,7 @@ class ListScraperV2:
             if item['source'] in __class_name_v2__.keys():
                 print("=> A scraper !!!")
                 try:
+                    print(item['url'])
                     instance = __class_name_v2__[item['source']](
                         url=item['url'], establishment=item['establishment_id'], settings=item['id'], env=self.env)
 
