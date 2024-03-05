88531fe80c83320f4b10e3558c05f1f78af148ac
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-03-05 11:04:37 +0300

    debug meteo

--- meteo-scraper/__main__.py
+++ meteo-scraper/__main__.py
@@ -90,7 +90,7 @@ class MeteoAPI(object):
                     timeout=120
                 )
 
-            print(response.json())
+            print(response.data)
 
             return orjson.loads(response.data)
 
