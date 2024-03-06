87c09ed7db67d1c86d40abfe016516e7052b731f
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-03-06 18:50:05 +0300

    youtube

--- social-media-scraper/youtube_scraper.py
+++ social-media-scraper/youtube_scraper.py
@@ -1,8 +1,9 @@
 import json
 import os
+from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 from nested_lookup import nested_lookup
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 from scraping import Scraping
 import re
@@ -50,157 +51,194 @@ class YoutubeProfileScraper(Scraping):
     def set_history(self, key: str, value: any) -> None:
         pass
 
-    def resolve_loginform(self) -> None:
-        self.fill_loginform()
-
     def goto_login(self) -> None:
         self.page.goto("https://www.youtube.com", timeout=30000)
-        self.page.wait_for_timeout(30000)
-
-    def fill_loginform(self) -> None:
-        self.page.wait_for_selector("[name='username']", timeout=30000)
-        self.page.locator("[name='username']").click()
-        time.sleep(.5)
-        self.page.fill("[name='username']", self.current_credential['email'])
-        time.sleep(.3)
-        self.page.locator("[name='password']").click()
-        time.sleep(.2)
-        self.page.fill("[name='password']",
-                       self.current_credential['password'])
-        time.sleep(.1)
-        self.page.locator("[type='submit']").click()
-        self.page.wait_for_timeout(70000)
-
-    def intercept_response(self, response) -> None:
-        """capture all background requests and save them"""
-        response_type = response.request.resource_type
-
-        if response_type == "fetch":
-
-            # => /next?prettyPrint=false
-            self.xhr_comments.append(response.url)
-
-            # if 'guide?prettyPrint=false' in response.url:
-            #     self.xhr_page = response.json()
-
-            if 'next?prettyPrint=false' in response.url:
-                self.xhr_posts.append(nested_lookup(
-                    key="compactVideoRenderer", document=response.json()))
+        # self.page.wait_for_timeout(30000)
 
     def open_posts(self):
-        posts_items = self.page.locator(
-            "ytd-rich-item-renderer ytd-rich-grid-media ytd-thumbnail a").all()
+        try:
+            posts_items = self.page.locator(
+                "ytd-rich-item-renderer ytd-rich-grid-media").all()
 
-        for item in posts_items:
-            href = item.get_attribute("href")
-            self.page.goto(f"https://www.youtube.com{href}", timeout=30000)
-            self.page.wait_for_timeout(30000)
-            self.scroll_down_page()
+            items = [item.locator('ytd-thumbnail a').get_attribute("href")
+                     for item in posts_items]
+            items_date = [item.locator(
+                'div#metadata span.inline-metadata-item:last-of-type').text_content() for item in posts_items]
+
+            for index in range(len(items)):
+                self.page.goto(
+                    f"https://www.youtube.com{items[index]}", timeout=30000)
+                self.page.wait_for_timeout(20000)
+                self.scroll_down_page(iteration=1, step=500)
+                self.page.wait_for_timeout(20000)
+                self.scroll_down_page(iteration="infinite")
+                self.page.wait_for_timeout(20000)
+                self.extract_post_data(date=items_date[index])
+
+            self.page_data['posts'] = len(self.posts)
+        except Exception as e:
+            print(e)
+            pass
 
     def complete_source_data(self):
 
-        posts = nested_lookup(key='node', document=self.xhr_posts)
+        posts = []
+
         coms = self.xhr_comments
 
-        for index in range(len(coms)):
-            try:
-                pk = coms[index]['caption']['pk']
-
-                print(pk)
-
-                if pk != posts[index]['caption']['pk']:
-                    for post in posts:
-                        if "caption" in post.keys() and post["caption"]["pk"] == pk:
-                            post["comments"] = coms[index]
-                else:
-                    posts[index]["comments"] = coms[index]
-
-            except Exception as e:
-                print(e)
-                pass
+        for item in self.xhr_posts:
+            for p in item:
+                posts.append(p)
 
         self.xhr_posts = posts
 
     def goto_youtube_page(self) -> None:
-        self.page.on("response", self.intercept_response)
         self.page.goto(f"{self.url}/videos", timeout=50000)
+
         self.page.wait_for_timeout(6000)
         time.sleep(3)
-        self.scroll_down_page()
+        self.extract_page_data()
+        print(self.page_data)
+        self.scroll_down_page(iteration="infinite")
         self.open_posts()
         # self.complete_source_data()
 
-    def scroll_down_page(self) -> None:
-        for i in range(5):  # make the range as long as needed
-            print("scrolling down ...")
-            self.page.mouse.wheel(0, 15000)
-            time.sleep(5)
+    def format_number(self, number):
+        splited_value = number.split(
+            '\xa0') if '\xa0' in number else number.split(' ')
+        if len(splited_value) == 3:
+            if splited_value[1].lower() == "k":
+                return int(float(splited_value[0].replace(',', '.'))*1000)
+            if splited_value[1].lower() == 'm':
+                return int(float(splited_value[0].replace(',', '.'))*1000000)
+        if len(splited_value) == 2:
+            if 'm ' in splited_value[1].lower():
+                return int(float(splited_value[0].replace(',', '.'))*1000000)
 
-    def get_json_content(self, item: object, key: str) -> object:
-        try:
-            return item[key]
-        except KeyError:
-            return ''
+            return int(float(splited_value[0].replace(',', '.')))
 
-    def extract_data(self) -> None:
+        return splited_value[0].replace(',', '.')
 
-        with open(f"{environ.get('SOCIAL_FOLDER')}/{self.establishment}_youtubeposts.json", 'w') as foutput:
-            json.dump(self.xhr_posts, foutput, indent=4, sort_keys=True)
+    def scroll_down_page(self, iteration='infinite', step=15000) -> None:
 
-        # with open(f"{environ.get('SOCIAL_FOLDER')}/{self.establishment}_youtubefetchs.json", 'w') as foutput:
-        #     json.dump(self.xhr_comments, foutput, indent=4, sort_keys=True)
+        if iteration == 'infinite':
+            prev_height = None
 
-        # with open(f"{environ.get('SOCIAL_FOLDER')}/{self.establishment}_youtubepage.json", 'w') as foutput:
-        #     json.dump(self.xhr_page, foutput, indent=4, sort_keys=True)
+            while True:
+                self.page.mouse.wheel(0, step)
+                time.sleep(5)
 
-        posts = self.xhr_posts
+                curr_height = self.page.evaluate(
+                    '(window.innerHeight + window.scrollY)')
 
-        # for post in posts:
+                if not prev_height:
+                    prev_height = curr_height
+                elif prev_height == curr_height:
+                    break
+                else:
+                    prev_height = curr_height
+        else:
 
-        #     if type(post["comments"]) != list:
+            for i in range(iteration):  # make the range as long as needed
+                self.page.mouse.wheel(0, step)
+                time.sleep(5)
 
-        #         comments = post["comments"]["comments"]
-        #         comment_values = []
-
-        #         for cmt in comments:
-        #             try:
-        #                 comment_values.append({
-        #                     'comment': cmt["text"],
-        #                     'published_at': datetime.fromtimestamp(cmt["created_at"]).strftime("%d/%m/%Y"),
-        #                     'likes': cmt["comment_like_count"],
-        #                     'author': cmt["user"]["full_name"]
-        #                 })
-        #             except Exception as e:
-        #                 print(e)
-        #                 pass
-
-        #         try:
-        #             self.posts.append({
-        #                 "author": post["owner"]["full_name"],
-        #                 "publishedAt": datetime.fromtimestamp(post["comments"]["caption"]["created_at"]).strftime("%d/%m/%Y"),
-        #                 "description": post["caption"]["text"],
-        #                 "reaction": post["like_count"],
-        #                 "comments": len(comment_values),
-        #                 "shares": 0,
-        #                 "hashtag": "",
-        #                 "comment_values": comment_values
-        #             })
-
-        #         except Exception as e:
-        #             print(e)
-        #             pass
-
-        print("posts traités: ", len(self.posts))
+    def extract_page_data(self) -> None:
 
         try:
+            soupe = BeautifulSoup(self.page.content(), 'lxml')
+            name = soupe.find('yt-formatted-string', {'class': "ytd-channel-name"}).text.strip() \
+                if soupe.find('yt-formatted-string', {'class': "ytd-channel-name"}) else ''
+            followers = soupe.find('yt-formatted-string', {'id': "subscriber-count"}).text.strip() \
+                if soupe.find('yt-formatted-string', {'id': "subscriber-count"}) else ''
+
             self.page_data = {
-                'followers': nested_lookup(key='edge_followed_by', document=self.xhr_page)[0]["count"],
+                'followers': self.format_number(followers),
                 'likes': 0,
-                'source': "instagram",
+                'source': "youtube",
                 'establishment': self.establishment,
-                'name': f"instagram_{nested_lookup(key='full_name', document=self.xhr_page)[0]}",
-                'posts': len(self.posts)
+                'name': f"youtube_{name}",
             }
+
+        except Exception as e:
+            print(e)
+            pass
+
+    def extract_post_data(self, date) -> None:
+        def format_date(string_date):
+            day_terms = ['day', 'days', 'jour', 'jours']
+            today_terms = ['seconde', 'secondes', 'minute',
+                           'minutes', 'hours', 'hour', 'heure', 'heures']
+            week_terms = ['week', 'weeks', 'semaine', 'semaines']
+            month_terms = ['month', 'months', 'mois']
+            year_terms = ['year', 'years', 'an', 'ans']
+
+            if string_date != "":
+
+                splited_date = string_date.lower().split(' ')
+
+                value = list(filter(lambda x: x.isnumeric(), splited_date))[0]
+
+                try:
+
+                    if any(x in today_terms for x in splited_date):
+                        return datetime.now()
+
+                    if any(x in day_terms for x in splited_date):
+                        return datetime.now() + timedelta(days=-int(value))
+
+                    if any(x in week_terms for x in splited_date):
+                        return datetime.now() + timedelta(weeks=-int(value))
+
+                    if any(x in month_terms for x in splited_date):
+                        return datetime.now() + timedelta(days=-int(value)*30)
+
+                    if any(x in year_terms for x in splited_date):
+                        return datetime.now() + timedelta(days=-int(value)*365)
+
+                    else:
+                        print("else")
+
+                except Exception as e:
+                    print(e)
+
+        soupe = BeautifulSoup(self.page.content(), 'lxml')
+
+        try:
+            title = soupe.find('ytd-watch-metadata', {'class': 'watch-active-metadata'}).find('div', {'id': "title"}).text.strip() \
+                if soupe.find('ytd-watch-metadata', {'class': 'watch-active-metadata'}) and soupe.find('ytd-watch-metadata', {'class': 'watch-active-metadata'}).find('div', {'id': "title"}) else ''
+            likes = soupe.find('like-button-view-model').find('div', {'class': "yt-spec-button-shape-next__button-text-content"}).text.strip() \
+                if soupe.find('like-button-view-model') and soupe.find('like-button-view-model').find('div', {'class': "yt-spec-button-shape-next__button-text-content"}) else ''
+            author = soupe.find('ytd-channel-name', {'id': 'channel-name'}).text.strip() \
+                if soupe.find('ytd-channel-name', {'id': 'channel-name'}) else ""
+            published_date = format_date(date).strftime('%d/%m/%Y')
+
+            comments = soupe.find_all('ytd-comment-thread-renderer')
+
+            comment_values = []
+
+            for comment in comments:
+                try:
+                    comment_values.append({
+                        'comment': comment.find('yt-formatted-string', {'id': 'content-text'}).text.strip() if comment.find('yt-formatted-string', {'id': 'content-text'}) else "",
+                        'published_at': format_date(comment.find('yt-formatted-string', {'class': 'published-time-text'}).text.strip()).strftime("%d/%m/%Y") if comment.find('yt-formatted-string', {'class': 'published-time-text'}) else "",
+                        'likes': self.format_number(comment.find('span', {'id': 'vote-count-middle'}).text.strip()) if comment.find('span', {'id': 'vote-count-middle'}) else 0,
+                        'author': comment.find('a', {'id': 'author-text'}).text.strip() if comment.find('a', {'id': 'author-text'}) else ""
+                    })
+                except Exception as e:
+                    print(e)
+                    pass
+
+            self.posts.append({
+                "author": author,
+                "publishedAt": published_date,
+                "description": title,
+                "reaction": likes,
+                "comments": len(comment_values),
+                "shares": 0,
+                "hashtag": "",
+                "comment_values": comment_values
+            })
 
         except Exception as e:
             print(e)
@@ -215,28 +253,23 @@ class YoutubeProfileScraper(Scraping):
         # progress.next()
         print(" | Open login page")
         self.goto_login()
-        # progress.next()
-        # print(" | Fill login page")
-        # self.fill_loginform()
-        # # progress.next()
-        # print(" | Logged in!")
         output_files = []
         for item in self.items:
-            # p_item = FillingCirclesBar(item['establishment_name'], max=3)
+            p_item = FillingCirclesBar(item['establishment_name'], max=3)
             self.set_item(item)
             self.clean_data()
-            # p_item.next()
+            p_item.next()
             print(" | Open page")
             self.goto_youtube_page()
+            p_item.next()
+            # print(" | Extracting")
+            # self.extract_data()
         #     p_item.next()
-            print(" | Extracting")
-            self.extract_data()
-        #     p_item.next()
-        #     print(" | Saving")
-        #     output_files.append(self.save())
-        #     p_item.next()
-        #     print(" | Saved")
+            print(" | Saving")
+            output_files.append(self.save())
+            p_item.next()
+            print(" | Saved")
 
         self.stop()
 
-        # return output_files
+        return output_files
