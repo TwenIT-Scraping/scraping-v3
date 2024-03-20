e3a49659cc08f2d48f5c65747a4859c8a0643143
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-03-20 21:55:48 +0300

    activer les changements

--- review-categorizer/__main__.py
+++ review-categorizer/__main__.py
@@ -365,16 +365,21 @@ class ClassificationAPI(object):
                 print("============> Page ", self.page,
                       "sur ", self.pages, " <===============")
                 self.fetch_datas()
-                # if len(self.categories):
-                #     self.update_lines()
-                #     # res = self.transform_data()
-                #     # print(res)
-                #     if os.environ.get('ENV_TYPE') != 'local':
-                #         res = self.upload()
-                #         print(res)
-                # else:
-                #     print("!!!! Pas de catégories")
-                #     break
+                if self.column == "category":
+                    if len(self.categories):
+                        self.update_lines()
+                        # res = self.transform_data()
+                        # print(res)
+
+                    else:
+                        print("!!!! Pas de catégories")
+                        break
+                else:
+                    self.update_lines()
+
+                if os.environ.get('ENV_TYPE') != 'local':
+                    res = self.upload()
+                    print(res)
 
                 print(len(self.lines))
 
