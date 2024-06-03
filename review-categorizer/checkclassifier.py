55334d636784352ee73cae41ca8698893d25d6ca
Johary97 <joharyandrianjafimanohisolo@gmail.com>
2024-06-03 13:32:24 +0200

    tester les catégories une par une

--- review-categorizer/checkclassifier.py
+++ review-categorizer/checkclassifier.py
@@ -3,21 +3,33 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipe
 
 def checkclassifier(categories, text):
     try:
+        prediction = {'séquence': text, 'labels': [], 'scores': [] }  
         classifier = pipeline(task="zero-shot-classification",
                               device=-1, model="facebook/bart-large-mnli")
+        
+        # categs = list(map(lambda x: x['category'], categories))
+        categs = categories
+        
+        for categ in categs:
+            result = classifier(text, categ, multi_label=False)
 
-        prediction = classifier(
-            text, list(map(lambda x: x['category'], categories)), multi_label=False)
+            print(result)
+            prediction['labels'].append(categ)
+            prediction['scores'].append(result['scores'][0])
 
-        print(prediction)
+        # prediction = classifier(
+           # text, list(map(lambda x: x['category'], categories)), multi_label=False)
+            # text, categories, multi_label=True)
+
+        print('\n', prediction)
 
     except Exception as e:
         print(e)
         pass
 
 
-categs = []
-texts = []
+categs = ['Mobilier', 'Ménage', 'propreté']
+texts = ['« Ménage parfait »: Les équipements, la propreté, le calme et la situation géographique. Le style des bâtiments. | Un petit entretien du mobilier des boiseries et autres']
 
 for t in texts:
     checkclassifier(categs, t)
