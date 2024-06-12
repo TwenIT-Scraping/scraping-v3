f77d6391b58684bc782ce95649ca2c5e0972560e
Thierry Keller <83826994+thierryKeller-1@users.noreply.github.com>
2024-06-12 18:13:26 +0300

    Update googles.py

--- review-scraper/googles.py
+++ review-scraper/googles.py
@@ -314,6 +314,11 @@ class Google(BaseGoogleScrap):
                             comment = comment.split("(original)")[-1]
                     except:
                         comment = ""
+                    url = ''
+                    try:
+                        url = card.find('a', {'class':'YhR3n'}, href=True)['href']
+                    except:
+                        url = self.driver.current_url
 
                     rating = card.find('div', {'class': 'GDWaad'}).text.strip() if card.find('div', {'class': 'GDWaad'}) else rating
                     date_raw = card.find('span', {'class': 'iUtr1 CQYfx'}).text.lower()
