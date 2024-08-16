37c6bc98a4d229878662efcefe7f3602874520b9
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-08-16 15:05:52 +0300

    catégorisation v2 with all functions

--- review-categorizer/checkclassifier.py
+++ review-categorizer/checkclassifier.py
@@ -1,12 +1,11 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
-from progress.bar import FillingCirclesBar
 import spacy
-
 
 ###################################################
 # Ségmentation de texte utilisant la librairie spacy qui consiste à découper
 # le texte lorsqu'on croire les adverbes, poctuations et conjonctions #
 ###################################################
+
 
 def segment_text(text):
     nlp = spacy.load("en_core_web_sm")
@@ -17,16 +16,15 @@ def segment_text(text):
 
     result = []
 
-    excepted_token = ["AND", "ET"]
+    excepted_token = ["AND", "ET", "THAT"]
 
     for token in doc:
         # print(token.text, " => ", token.pos_)
 
-        if token.pos_ in ["PUNCT", "CCONJ", "ADV"] and token.text.upper() not in excepted_token:
-            # print(f"Separator found: {token.text}")
+        if (token.pos_ in ["PUNCT", "CCONJ", "ADV"] and token.text.upper() not in excepted_token) or token.text == "|":
 
             if sentence != "":
-                if token.text == "," and word_count >= 3:
+                if (token.text == "," or token.text == "|") and word_count >= 3:
                     result.append(sentence)
                     sentence = ""
                     word_count = 0
@@ -65,12 +63,13 @@ def segment_by_length(text, max_length=512):
             line += " " + term
         else:
             segments.append(line.strip())
-            line = ""
+            line = term
 
     if line != "":
         segments.append(line.strip())
 
     return segments
+
 ###################################################
 # Segment the text and categorize each part #
 # Expected result: [(term, [category1, category2, ...]), ...] #
@@ -83,7 +82,7 @@ def detect_aspect_category(text, candidate_labels, score_min=.8, full_text=False
 
     def get_labels(data, score_min):
         labels_with_score = list(zip(data['labels'], data['scores']))
-        filtered_labels = [label for label,
+        filtered_labels = [(label, score) for label,
                            score in labels_with_score if score >= score_min]
         return filtered_labels
 
@@ -108,6 +107,7 @@ def detect_aspect_category(text, candidate_labels, score_min=.8, full_text=False
 
     for term in aspect_terms:
         result = classifier(term, candidate_labels, multi_label=True)
+        print(result)
 
         ####### result format ########
         # {
@@ -122,9 +122,9 @@ def detect_aspect_category(text, candidate_labels, score_min=.8, full_text=False
         top_categories = get_labels(result, score_min)
         sentence_categories.append((term, top_categories))
 
-    categories.append(sentence_categories)
+    # categories.append(sentence_categories)
 
-    return categories
+    return sentence_categories
 
 #######################################################
 # Compute global categories then categorize each term using them #
@@ -132,17 +132,94 @@ def detect_aspect_category(text, candidate_labels, score_min=.8, full_text=False
 #######################################################
 
 
-def get_categories(text, score_min, labels):
+def get_categories(text, labels):
     global_aspect_categories = detect_aspect_category(
-        text, labels, score_min, True)
+        text, labels, 0.6, True)
     global_categories = []
 
     for aspect_category in global_aspect_categories:
         for category in aspect_category[1]:
-            if category not in global_categories:
-                global_categories.append(category)
+            if category[0] not in global_categories:
+                global_categories.append(category[0])
+
+    if len(global_categories) == 0:
+        return []
 
     aspect_categories = detect_aspect_category(
-        text, global_categories, score_min, False)
+        text, global_categories, 0.8, False)
 
     return aspect_categories
+
+
+def concat_sequences_by_label(categories, score_min=0.8):
+    """
+    Concatenates sequences for each label with a score greater than 0.8.
+
+    Args:
+      categories: A list of tuples, where each tuple contains a sequence and a list of (label, score) pairs.
+
+    Returns:
+      A dictionary mapping labels to concatenated sequences.
+    """
+
+    label_sequences = {}
+
+    for sequence, label_scores in categories:
+        for label, score in label_scores:
+            if score >= score_min:
+                if label not in label_sequences:
+                    label_sequences[label] = []
+                    label_sequences[label].append((sequence, score))
+                else:
+                    label_sequences[label].append((sequence, score))
+
+    for label, sequences in label_sequences.items():
+        score_avg = 0
+
+        for sequence, score in sequences:
+            score_avg += score
+        score_avg /= len(sequences)
+
+        label_sequences[label] = (" | ".join([seq[0]
+                                  for seq in sequences]), score_avg)
+
+    return label_sequences
+
+# Example usage:
+# candidate_labels = ['Emplacement', 'Accueil', 'Ménage', 'Mobilier']
+# text = "« Repos et nature, les Alpes en face au petit déjeuner sur le balcon. »: Le lieu, en plein causse des Bauges. Les balades proches. Les lieux touristiques: Aix les Bains 30 min, Annecy 50 min. | Le nettoyage. Certains chalets devraient être réservés aux voyageurs possédant des animaux. Notamment pour les personnes ayant des allergies."
+# categories = get_categories(text, candidate_labels)
+# concatenated_sequences = concat_sequences_by_label(categories, 0.8)
+# print(concatenated_sequences)
+
+
+def transform_to_json(concatenated_sequences):
+    """
+    Transforms the result of concat_sequences_by_label to a JSON object.
+
+    Args:
+      concatenated_sequences: A dictionary mapping labels to concatenated sequences and their average scores.
+
+    Returns:
+      A list of dictionaries, where each dictionary represents a category with its sequence and score.
+    """
+
+    json_result = []
+
+    for category, (sequence, score) in concatenated_sequences.items():
+        json_result.append({
+            "category": category,
+            "sequence": sequence,
+            "score": score
+        })
+
+    return json_result
+
+# Example usage:
+# candidate_labels = ['Emplacement', 'Accueil', 'Ménage', 'Mobilier']
+# text = "« Repos et nature, les Alpes en face au petit déjeuner sur le balcon. »: Le lieu, en plein causse des Bauges. Les balades proches. Les lieux touristiques: Aix les Bains 30 min, Annecy 50 min. | Le nettoyage. Certains chalets devraient être réservés aux voyageurs possédant des animaux. Notamment pour les personnes ayant des allergies."
+# categories = get_categories(text, candidate_labels)
+# concatenated_sequences = concat_sequences_by_label(categories, 0.8)
+# print(concatenated_sequences)
+# json_output = transform_to_json(concatenated_sequences)
+# print(json_output)
