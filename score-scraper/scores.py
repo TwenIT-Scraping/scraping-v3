1c025a536097b8dbfef05d1669e8520632031dea
Miavaka <miavaka80@gmail.com>
2024-12-04 15:04:26 +0300

    score-scraper google: ajout time.sleep

--- score-scraper/scores.py
+++ score-scraper/scores.py
@@ -179,7 +179,7 @@ def score_scraping_task(driver: Driver, data:list, env:str='PROD'):
         try:
             button_accept_before_start_run_scrap = "#yDmH0d > c-wiz > div > div > div > div.NIoIEf > div.G4njw > div.AIC7ge > div.CxJub > div.VtwTSb > form:nth-child(2) > div > div > button"
             driver.click(button_accept_before_start_run_scrap, wait=Wait.SHORT)
-            time.sleep(random.randint(1,2))
+            time.sleep(random.randint(3,4))
         except Exception as e:
             print(f'erreur => {e}')
     if page_type == 'unknown':
