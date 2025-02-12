d4d6a62b05f1c6ab4366903c151f5a9880778737
Miavaka <miavaka80@gmail.com>
2025-02-12 12:00:09 +0300

    Score google: MAJ selecteur accept cookie (3eme type)

--- score-scraper/scores.py
+++ score-scraper/scores.py
@@ -191,14 +191,20 @@ def score_scraping_task(driver: Driver, data:list, env:str='PROD'):
                 # button_accept_before_start_run_scrap = "#yDmH0d > c-wiz > div > div > div > div.NIoIEf > div.G4njw > div.AIC7ge > div.CxJub > div.VtwTSb > form:nth-child(2) > div > div > button"
                 driver.click(button_accept_before_start_run_scrap, wait=Wait.SHORT)
                 time.sleep(random.randint(3,4))
-            except Exception as error:
+            except Exception as error0:
+                print(f'1er selecteur accept cookie not found => {error0}')
                 try:
-                    button_accept_before_start_run_scrap = "#L2AGLb"
+                    button_accept_before_start_run_scrap = "#yDmH0d > c-wiz > div > div > div > div.NIoIEf > div.jn2Duf > div.AIC7ge > div.CxJub > div.VtwTSb > form:nth-child(2) > div > div > button"
                     driver.click(button_accept_before_start_run_scrap, wait=Wait.SHORT)
                     time.sleep(random.randint(3,4))
-                except Exception as e:
-                    print(f"2eme selecteur accept cookie not found => {e}")
-                print(f'1er selecteur accept cookie not found => {error}')
+                except Exception as error1:
+                    print(f"2eme selecteur accept cookie not found => {error1}")
+                    try:
+                        button_accept_before_start_run_scrap = "#L2AGLb"
+                        driver.click(button_accept_before_start_run_scrap, wait=Wait.SHORT)
+                        time.sleep(random.randint(3,4))
+                    except Exception as e:
+                        print(f"3eme selecteur accept cookie not found => {e}")
                 pass
         if page_type == 'unknown':
             print("selector not define for this page")
